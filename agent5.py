e1ec212d5ec7eeb07be883476ff43802c35c5eea
Shushang1999 <shushang.nair@gmail.com>
2022-10-14 05:39:23 -0400

    Agent 5 enhanced

--- agent5.py
+++ agent5.py
@@ -21,16 +21,15 @@ def agent5(no_of_ghosts):
     while True:
         # print(path_taken_by_agent)
         path = maze_check.bfs(grid,(x2,y2))  # Check for valid Paths
-        print(grid)
         for point in path:
             potential_x,potential_y = point
-            ghost_in_neighboring_cells = False
+            ghost_blocked_in_neighboring_cells = False
             for neighbor_x,neighbor_y in (potential_x+1,potential_y),(potential_x-1,potential_y),(potential_x,potential_y+1),(potential_x,potential_y-1),(potential_x,potential_y):
                 if neighbor_x < 0 or neighbor_x >= 51 or neighbor_y < 0 or neighbor_y >= 51:
                     continue
-                if grid[neighbor_x][neighbor_y] == 8 or grid[neighbor_x][neighbor_y] >= 10:
-                    ghost_in_neighboring_cells = True
-            if ghost_in_neighboring_cells == False:
+                if grid[neighbor_x][neighbor_y] == 8 or grid[neighbor_x][neighbor_y] == 0 or grid[neighbor_x][neighbor_y] >= 10:
+                    ghost_blocked_in_neighboring_cells = True
+            if ghost_blocked_in_neighboring_cells == False:
                 x2,y2 = point
                 path_taken_by_agent.append((x2,y2))
                 if grid[x2][y2] == 8 or grid[x2][y2] >= 10:         # Checking If Agent Dead 
@@ -43,39 +42,39 @@ def agent5(no_of_ghosts):
                     if grid[x2][y2] == 8 or grid[x2][y2] >= 10:         # Checking If Agent Dead 
                         return ("Failed")
             else:
-                for new_x,new_y in (x2+1,y2),(x2-1,y2),(x2,y2+1),(x2,y2-1):
-                    if new_x < 0 or new_x > 50 or new_y < 0 or new_y > 50:
+                ghost_in_neighboring_cells = False
+                for neighbor_x,neighbor_y in (potential_x+1,potential_y),(potential_x-1,potential_y),(potential_x,potential_y+1),(potential_x,potential_y-1),(potential_x,potential_y):
+                    if neighbor_x < 0 or neighbor_x >= 51 or neighbor_y < 0 or neighbor_y >= 51:
                         continue
-                    if grid[new_x][new_y] == 0:
-                        continue
+                    if grid[neighbor_x][neighbor_y] == 8 or grid[neighbor_x][neighbor_y] >= 10:
+                        ghost_in_neighboring_cells = True
+                if ghost_in_neighboring_cells == False:
+                    x2,y2 = point
+                    path_taken_by_agent.append((x2,y2))
+                    if grid[x2][y2] == 8 or grid[x2][y2] >= 10:         # Checking If Agent Dead 
+                        return ("Failed")
+                    elif grid[x2][y2] == 2:
+                        globalVariables.success_count = globalVariables.success_count + 1
+                        return("Success")
                     else:
-                        blocked_ghosts_neighbor = False
-                        for neighbor_x,neighbor_y in (new_x+1,new_y),(new_x-1,new_y),(new_x,new_y+1),(new_x,new_y-1),(new_x,new_y):
-                            if neighbor_x < 0 or neighbor_x >= 51 or neighbor_y < 0 or neighbor_y >= 51:
-                                continue
-                            if grid[neighbor_x][neighbor_y] == 8 or grid[neighbor_x][neighbor_y] == 0  or grid[neighbor_x][neighbor_y] >= 10:
-                                blocked_ghosts_neighbor = True
-                                break
-                        if blocked_ghosts_neighbor == False:
-                            x2,y2 = new_x,new_y
-                            if grid[x2][y2] == 8 or grid[x2][y2] >= 10:         # Checking If Agent Dead 
-                                return ("Failed")
-                            elif grid[x2][y2] == 2:
-                                globalVariables.success_count = globalVariables.success_count + 1
-                                return("Success")
-                            else:
-                                grid, ghosts_coordinate = ghosts.move_ghosts(grid,ghosts_coordinate)
-                                if grid[x2][y2] == 8 or grid[x2][y2] >= 10:         # Checking If Agent Dead 
-                                    return ("Failed")
+                        grid, ghosts_coordinate = ghosts.move_ghosts(grid,ghosts_coordinate)
+                        if grid[x2][y2] == 8 or grid[x2][y2] >= 10:         # Checking If Agent Dead 
+                            return ("Failed")
+                else:
+                    for new_x,new_y in (x2+1,y2),(x2-1,y2),(x2,y2+1),(x2,y2-1):
+                        if new_x < 0 or new_x > 50 or new_y < 0 or new_y > 50:
+                            continue
+                        if grid[new_x][new_y] == 0:
+                            continue
                         else:
-                            ghosts_neighbor = False
+                            blocked_ghosts_neighbor = False
                             for neighbor_x,neighbor_y in (new_x+1,new_y),(new_x-1,new_y),(new_x,new_y+1),(new_x,new_y-1),(new_x,new_y):
                                 if neighbor_x < 0 or neighbor_x >= 51 or neighbor_y < 0 or neighbor_y >= 51:
                                     continue
-                                if grid[neighbor_x][neighbor_y] == 8 or grid[neighbor_x][neighbor_y] >= 10:
-                                    ghosts_neighbor = True
+                                if grid[neighbor_x][neighbor_y] == 8 or grid[neighbor_x][neighbor_y] == 0  or grid[neighbor_x][neighbor_y] >= 10:
+                                    blocked_ghosts_neighbor = True
                                     break
-                            if ghosts_neighbor == False:
+                            if blocked_ghosts_neighbor == False:
                                 x2,y2 = new_x,new_y
                                 if grid[x2][y2] == 8 or grid[x2][y2] >= 10:         # Checking If Agent Dead 
                                     return ("Failed")
@@ -87,30 +86,49 @@ def agent5(no_of_ghosts):
                                     if grid[x2][y2] == 8 or grid[x2][y2] >= 10:         # Checking If Agent Dead 
                                         return ("Failed")
                             else:
-                                while True:
-                                    new_x,new_y = random.choice([(x2+1,y2),(x2-1,y2),(x2,y2+1),(x2,y2-1)])
-                                    if new_x < 0 or new_x >= 51 or new_y < 0 or new_y >= 51:
+                                ghosts_neighbor = False
+                                for neighbor_x,neighbor_y in (new_x+1,new_y),(new_x-1,new_y),(new_x,new_y+1),(new_x,new_y-1),(new_x,new_y):
+                                    if neighbor_x < 0 or neighbor_x >= 51 or neighbor_y < 0 or neighbor_y >= 51:
                                         continue
-                                    if grid[new_x][new_y] == 0:
-                                        continue
-                                    else:
+                                    if grid[neighbor_x][neighbor_y] == 8 or grid[neighbor_x][neighbor_y] >= 10:
+                                        ghosts_neighbor = True
                                         break
-                                x2,y2 = new_x,new_y
-                                if grid[x2][y2] == 8 or grid[x2][y2] >= 10:         # Checking If Agent Dead 
-                                    return ("Failed")
-                                elif grid[x2][y2] == 2:
-                                    globalVariables.success_count = globalVariables.success_count + 1
-                                    return("Success")
-                                else:
-                                    grid, ghosts_coordinate = ghosts.move_ghosts(grid,ghosts_coordinate)
+                                if ghosts_neighbor == False:
+                                    x2,y2 = new_x,new_y
                                     if grid[x2][y2] == 8 or grid[x2][y2] >= 10:         # Checking If Agent Dead 
                                         return ("Failed")
+                                    elif grid[x2][y2] == 2:
+                                        globalVariables.success_count = globalVariables.success_count + 1
+                                        return("Success")
+                                    else:
+                                        grid, ghosts_coordinate = ghosts.move_ghosts(grid,ghosts_coordinate)
+                                        if grid[x2][y2] == 8 or grid[x2][y2] >= 10:         # Checking If Agent Dead 
+                                            return ("Failed")
+                                else:
+                                    while True:
+                                        new_x,new_y = random.choice([(x2+1,y2),(x2-1,y2),(x2,y2+1),(x2,y2-1)])
+                                        if new_x < 0 or new_x >= 51 or new_y < 0 or new_y >= 51:
+                                            continue
+                                        if grid[new_x][new_y] == 0:
+                                            continue
+                                        else:
+                                            break
+                                    x2,y2 = new_x,new_y
+                                    if grid[x2][y2] == 8 or grid[x2][y2] >= 10:         # Checking If Agent Dead 
+                                        return ("Failed")
+                                    elif grid[x2][y2] == 2:
+                                        globalVariables.success_count = globalVariables.success_count + 1
+                                        return("Success")
+                                    else:
+                                        grid, ghosts_coordinate = ghosts.move_ghosts(grid,ghosts_coordinate)
+                                        if grid[x2][y2] == 8 or grid[x2][y2] >= 10:         # Checking If Agent Dead 
+                                            return ("Failed")
                         break
                         
 # for _ in range(0,100):                
-#     print(agent4(100))  
+#     print(agent5(100))  
 if __name__ == "__main__": 
-    for no_of_ghosts in range(161,300,5):
+    for no_of_ghosts in range(1,300,5):
         for _ in range(1,100):
             output.append(agent5(no_of_ghosts))
         with open("./Results/Ghosts_invisible_in_walls/output_agent5.txt","a") as o:
